20a766eccfe57984130bc5ae4255bea8441dcd74
NK <45469562+navapro@users.noreply.github.com>
2021-01-06 10:12:55 -0600

    Create a3q2.py

---
+++ cmpt assingment 3/a3q2.py
@@ -0,0 +1,11 @@
+# Navaneeth Krishna Anilkumar
+# NSID: nka629
+# Student number: 11306665
+# DR. Mark Keil
+
+input_string = input("Please input a string :")
+
+odd_str = input_string[1::2]
+even_str = input_string[::2]
+
+print("The result by splitting the odd and even part of the input string is :", odd_str +","+ even_str)
